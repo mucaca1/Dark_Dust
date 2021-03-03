521ddd911bc75de2100403ffd7557d1028c176f5
mucaca1 <matejkrc1@gmail.com>
2021-03-03 11:39:54 +0100

    feat: Add card loading
    - Add all cards (Scriptable objects)
    - Add loading in game manager

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,9 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
-public class GameManager : NetworkBehaviour {
-    
-    
+namespace Game {
+    public class GameManager : NetworkBehaviour {
+        private PlaygroundCard[] _playgroundCards;
+
+        [ServerCallback]
+        private void Start() {
+            _playgroundCards = Resources.LoadAll<PlaygroundCard>("");
+
+            Debug.Log(_playgroundCards.Length == 0
+                ? "No playground cards was found. Check Resources folder"
+                : "Playground cards was loaded successfully");
+        }
+    }
 }
--- Assets/Scripts/Game/PlaygroundCard.cs
+++ Assets/Scripts/Game/PlaygroundCard.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 namespace Game {
-    [CreateAssetMenu(fileName = "new playgroud card", menuName = "DarkDust/Playground Card", order = 0)]
+    [CreateAssetMenu(fileName = "new_playgroud_card", menuName = "DarkDust/Playground Card", order = 0)]
     public class PlaygroundCard : ScriptableObject {
         [SerializeField] private PlaygroundCardType _cardType;
         [SerializeField] private CardDirection _cardDirection = CardDirection.None;
