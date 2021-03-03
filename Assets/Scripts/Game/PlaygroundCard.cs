84f18006532c17f6a8575a47aacc42324a7407d2
mucaca1 <matejkrc1@gmail.com>
2021-03-03 10:58:09 +0100

    feat: Add card direction
    - Add card direction to all cards

--- Assets/Scripts/Game/PlaygroundCard.cs
+++ Assets/Scripts/Game/PlaygroundCard.cs
@@ -4,6 +4,7 @@ namespace Game {
     [CreateAssetMenu(fileName = "new playgroud card", menuName = "DarkDust/Playground Card", order = 0)]
     public class PlaygroundCard : ScriptableObject {
         [SerializeField] private PlaygroundCardType _cardType;
+        [SerializeField] private CardDirection _cardDirection = CardDirection.None;
         [SerializeField] private Sprite _frontImage;
         [SerializeField] private Sprite _backImage;
         [SerializeField] private int _cardCount;
