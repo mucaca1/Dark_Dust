c1de5a0394538ddfb58bf1cf56465f2983465187
mucaca1 <matejkrc1@gmail.com>
2021-03-08 09:38:09 +0100

    feat: Show/Hide Dark Dust UI
    - Add ShowHideUI.cs

---
+++ Assets/Scripts/Game/UI/ShowHideUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Game.UI {
+    public class ShowHideUI : MonoBehaviour, IPointerClickHandler {
+
+        [SerializeField] private GameObject _showBar = null;
+        [SerializeField] private GameObject _barToShow = null;
+
+        [SerializeField] private bool _hideShowBar = false;
+        private bool _isShowed = false;
+
+        
+        public void OnPointerClick(PointerEventData eventData) {
+            _isShowed = !_isShowed;
+            _barToShow.SetActive(_isShowed);
+        }
+    }
+}
