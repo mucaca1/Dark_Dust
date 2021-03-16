20dd340e01cd3d275e9e4082643dfec9d5003c48
mucaca1 <matejkrc1@gmail.com>
2021-03-16 16:37:39 +0100

    feat: Difficulty
    - Add difficulty values

---
+++ Assets/Scripts/Game/Difficulty.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    [CreateAssetMenu(fileName = "New Difficulty", menuName = "DarkDust/Difficulty", order = 0)]
+    public class Difficulty : ScriptableObject {
+        public int players = 0;
+        public int[] dustPower = new int[0];
+    }
+}
